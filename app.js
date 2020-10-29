626e42e8647652f1da0937fdc9d278655475937c
Aleksey Kozlovskiy <aikozlovsky@yandex.ru>
2020-10-29 22:18:59 +1000

    feat: Добавь мидлвэр, если адрес get-запроса не существует

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use('/', users);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  res.status(404).send({ "message": "Запрашиваемый ресурс не найден" });
+})
+
 app.listen(PORT, () => {
   console.log(`Сервер работает на порте ${PORT}`);
 });
--- routes/cards.js
+++ routes/cards.js
@@ -2,7 +2,7 @@ const cards = require('express').Router();
 const fs = require('fs');
 const path = require('path');
 
-cards.get('/cards', (req, res) => {
+cards.get('/cards', (req, res, next) => {
   const filepath = path.join(__dirname, '../data/cards.json');
   fs.readFile(filepath, { encoding: 'utf8' }, (err, data) => {
     if (err) {
